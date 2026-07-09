db1c9cba7bc4863dae610596b100326dc8e9a099
FitiavanaLova <hei.fitiavana@gmail.com>
2026-07-09 08:19:08 +0300

    update CourseSubscriptionController

--- src/main/java/hei/school/demo/endpoint/rest/controller/CourseSubscriptionController.java
+++ src/main/java/hei/school/demo/endpoint/rest/controller/CourseSubscriptionController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class CourseSubscriptionController {
-
+//Subscription
   private final CourseSubscriptionService courseSubscriptionService;
   private final Mailer mailer;
 
